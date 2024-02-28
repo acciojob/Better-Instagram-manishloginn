7fb19bd85bf7f49e300d314da29d944260033fc6
manishloginn <132683386+manishloginn@users.noreply.github.com>
2024-02-28 22:59:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,42 @@
-//your code here
+ 
+let count = 1;
+let draggingElement = null;
+ 
+const imgs = document.querySelectorAll("img");
+imgs.forEach((e) => {
+  e.id = `drag${count++}`;
+});
+ 
+const images = document.querySelectorAll(".image");
+images.forEach((e) => {
+  e.addEventListener("dragstart", onDragStart);
+  e.addEventListener("dragover", onDragOver);
+  e.addEventListener("drop", onDrop);
+});
+ 
+function onDragStart(event) {
+  draggingElement = event.currentTarget;
+}
+ 
+function onDragOver(event) {
+  if (draggingElement.parentNode.id === event.currentTarget.id) {
+    return;
+  }
+  event.preventDefault();
+}
+ 
+function onDrop(event) {
+  const id = event.currentTarget.id;
+  const bgImg = event.currentTarget.style.backgroundImage;
+  const text = event.currentTarget.innerText;
+ 
+  event.currentTarget.id = draggingElement.id;
+  event.currentTarget.style.backgroundImage =
+    draggingElement.style.backgroundImage;
+  event.currentTarget.innerText = draggingElement.innerText;
+ 
+  draggingElement.id = id;
+  draggingElement.style.backgroundImage = bgImg;
+  draggingElement.innerText = text;
+  draggingElement = null;
+}
